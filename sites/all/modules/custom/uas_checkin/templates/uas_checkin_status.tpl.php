becbc5b7fe105c70b10e322e388a42a0f0092d69
Nicholas Hepner <nick@hepnermedia.com>
2012-07-13 00:27:01 +0000

    Added styling to checkin module.

--- sites/all/modules/custom/uas_checkin/templates/uas_checkin_status.tpl.php
+++ sites/all/modules/custom/uas_checkin/templates/uas_checkin_status.tpl.php
@@ -1,3 +1,5 @@
-<div class="uas-checkin-status-wrapper <?php print $status ? 'uas-checkin-status-open' : 'uas-checkin-status-closed'; ?>">
-	<?php print $msg; ?>
+<div class="uas-checkin-status-wrapper round <?php print $status ? 'uas-checkin-status-open' : 'uas-checkin-status-closed'; ?>">
+	<div class="uas-checkin-status-inner">
+		<?php print $msg; ?>
+	</div>
 </div>

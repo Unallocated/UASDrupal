c09214ef2aae48fa5f91f52c214da36c272313a6
Nicholas Hepner <nick@hepnermedia.com>
2012-07-15 03:23:25 +0000

    Added field collection stuff. Fixed remote file detection errors for checkin

--- sites/all/modules/custom/uas_checkin/templates/uas_checkin_status.tpl.php
+++ sites/all/modules/custom/uas_checkin/templates/uas_checkin_status.tpl.php
@@ -1,5 +1,5 @@
-<div class="uas-checkin-status-wrapper round <?php print $status ? 'uas-checkin-status-open' : 'uas-checkin-status-closed'; ?>">
+<div class="uas-checkin-status-wrapper round <?php print $checkin_status['class']; ?>">
 	<div class="uas-checkin-status-inner">
-		<?php print $msg; ?>
+		<?php print $checkin_status['msg']; ?>
 	</div>
 </div>

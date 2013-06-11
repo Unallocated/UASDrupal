3374342d401e351917fb61f7803f82afb063c1f5
nhepner1 <Nicholas_M_Hepner@oa.eop.gov>
2013-06-10 22:19:01 -0400

    [#10] Removed modules, themes, and libraries from old site

--- docroot/sites/all/modules/custom/uas_checkin/templates/uas_checkin_status.tpl.php
+++
@@ -1,5 +0,0 @@
-<div class="uas-checkin-status-wrapper round <?php print $checkin_status['class']; ?>">
-	<div class="uas-checkin-status-inner">
-		<?php print $checkin_status['msg']; ?>
-	</div>
-</div>

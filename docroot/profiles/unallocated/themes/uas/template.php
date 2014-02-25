25caa8a533c4c814f205fc773ef76cadd99f6469
Nick Hepner <nick@hepnermedia.com>
2014-02-24 22:35:55 -0500

    [#152] Added first round theming to the members directoryf

--- docroot/profiles/unallocated/themes/uas/template.php
+++ docroot/profiles/unallocated/themes/uas/template.php
@@ -13,19 +13,23 @@
 
 function uas_preprocess_views_exposed_form(&$vars, $hook) {
 
-  if ($vars['form']['#id'] == 'views-exposed-form-search-page') {
+  switch($vars['form']['#id']) {
 
-    $vars['form']['search_api_views_fulltext']['#default_value'] = t('Search'); // Set a default value for the textfield
-    $vars['form']['search_api_views_fulltext']['#attributes']['onblur'] = "if (this.value == '') {this.value = 'Search';}";
-    $vars['form']['search_api_views_fulltext']['#attributes']['onfocus'] = "if (this.value == 'Search') {this.value = '';}";
+    case 'views-exposed-form-members-page':
+    case 'views-exposed-form-search-page':
 
-    unset($vars['form']['search_api_views_fulltext']['#printed']);
-    $vars['widgets']['filter-search_api_views_fulltext']->widget = drupal_render($vars['form']['search_api_views_fulltext']);
+      $vars['form']['search_api_views_fulltext']['#default_value'] = t('Search'); // Set a default value for the textfield
+      $vars['form']['search_api_views_fulltext']['#attributes']['onblur'] = "if (this.value == '') {this.value = 'Search';}";
+      $vars['form']['search_api_views_fulltext']['#attributes']['onfocus'] = "if (this.value == 'Search') {this.value = '';}";
+
+      unset($vars['form']['search_api_views_fulltext']['#printed']);
+      $vars['widgets']['filter-search_api_views_fulltext']->widget = drupal_render($vars['form']['search_api_views_fulltext']);
 
 
-    $vars['form']['submit']['#value'] = t('Search');
-    $vars['form']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/images/icons/search-icon.png');
-    unset($vars['form']['submit']['#printed']);
-    $vars['button'] = drupal_render($vars['form']['submit']);
+      $vars['form']['submit']['#value'] = t('Search');
+      $vars['form']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/images/icons/search-icon.png');
+      unset($vars['form']['submit']['#printed']);
+      $vars['button'] = drupal_render($vars['form']['submit']);
+      break;
   }
 }

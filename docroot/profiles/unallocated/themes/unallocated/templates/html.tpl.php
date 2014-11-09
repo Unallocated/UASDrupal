bb2684600111316362e736cd6ed40dcb1a219080
Michael <michael@runninggun.com>
2014-11-08 21:29:46 -0500

    Base Theme Added

---
+++ docroot/profiles/unallocated/themes/unallocated/templates/html.tpl.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="en">
+<!--<![endif]-->
+<head>
+<?php print $head; ?>
+<meta charset="utf-8">
+<title><?php print $head_title; ?></title>
+<?php print $styles; ?>
+<?php print $scripts; ?>
+
+</head>
+<body class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>

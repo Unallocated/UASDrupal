53b8d6520c042ff159ae013f0d31bdff69065aad
r00ster <r00ster@unallocatedspace.org>
2015-05-04 14:23:05 -0400

    [#244] Pulling remaining changes from the 244-add-cheryl-theme

--- docroot/profiles/unallocated/themes/unallocated/templates/page.tpl.php
+++ docroot/profiles/unallocated/themes/unallocated/templates/page.tpl.php
@@ -8,62 +8,76 @@ $ColumnAWidth = '"five columns alpha"';
 $ColumnBWidth = '"eleven columns omega"';
 $ColumnBottomWidth ='"four columns"';
 
-?>
-
-<div id= 'top_zone'>
-  <div class= 'container'>
-    <?php if ($logo): ?>
+//Prints Heading based on Values
+print ("<div id= 'top_zone'>");
+print ("<div class= 'container'>");
+if ($logo): ?>
       <a href="<?php print $front_page; ?>" title="<?php  print t('Home'); ?>" rel="home" id="logo">
-        <img id="logoImg" src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        <img id="logoImg", src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
       </a>
     <?php endif; ?>
-
-  </div>
-  <div class= 'nav container'>
-    <?php print render($page['main_nav']); ?>
-  </div>
-</div>
-
-<div id='core' class="<?php print $fullWidth; ?>">
-  <div class= container>
-
-  <?php if($page['sidebar_first']): ?>
-    <div class="<?php print $ColumnAWidth; ?>">
-      <?php print render($page['sidebar_first']); ?>
-    </div>
-
-    <div class="<?php print $ColumnBWidth; ?>">
-      <?php print render($page['content']); ?>
-    </div>
-   <?php else: ?>
-      <?php print render($page['content']); ?>
-   <?php endif; ?>
-  </div>
-</div>
-
-<div id= 'bottom_zone'>
-	<div class= 'container'>
-
-		<div class="<?php print $ColumnBottomWidth; ?>">
-	    	<div id='boxes-box-uas_address_box'>
-				</div>
-		</div>
+    
+ <?php
+print ("</div>");
+print ("</div>");
+print ("<div class= 'nav container'>");
 
 
-		<div class="<?php print $ColumnBottomWidth; ?>">
-			<?php print render($page['postscript_first']); ?>
-		</div>
+//Special div for Navigation elements
+	print render($page['menu']);
 
+print ("</div>");
 
-		<div class="<?php print $ColumnBottomWidth; ?>">
-	    <?php print render($page['post_postscript']); ?>
-		</div>
+/*Main Content section, with Sidebar relivence check
 
+Breaks into one or two blocks, depending on circumstances*/
+print ("<div id= 'core' , class= $fullWidth>");
+print ("<div class= container>");
 
-		<div class="<?php print $ColumnBottomWidth; ?>">
-			<?php print render($page['post_post_postscript']); ?>
-		</div>
+ if($page['sidebar_first']) {
+	print ("<div class= $ColumnAWidth>");  
+	print render($page['sidebar_first']);
+	print ("</div>");
 	
-	</div>
-</div>
+	print ("<div class= $ColumnBWidth>");  	
+	print render($page['content']);	
+	print ("</div>");
+ }
+ 
+ else
+ 	{ 	
+	print render($page['content']);	
+	}
+	
+print ("</div>");
+print ("</div>");
+    
+//Bottom Section Devided between 4 zones. The Logo is temporarily hardwired, prior to discussion about how to handle 2nd logo management.
 
+print ("<div id= 'bottom_zone'>");
+	print ("<div class= 'container'>");
+
+
+		print ("<div class= $ColumnBottomWidth>");
+	    	print ("<div id='boxes-box-uas_address_box'>
+					</div>");	
+		print ("</div>");
+
+
+		print ("<div class= $ColumnBottomWidth>");	
+			print render($page['postscript_first']);	
+		print ("</div>");
+
+
+		print ("<div class= $ColumnBottomWidth>");
+			print render($page['post_postscript']);		
+		print ("</div>");
+
+
+		print ("<div class= $ColumnBottomWidth>");
+			print render($page['post_post_postscript']);
+		print ("</div>");
+	
+	print ("</div>");
+print ("</div>");
+?>  

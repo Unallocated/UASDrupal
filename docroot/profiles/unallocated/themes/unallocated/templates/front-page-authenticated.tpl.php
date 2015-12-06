8854d6cbd9785b6e3694ce07a0b2fb1fc059b193
r00ster <nick@hepnermedia.com>
2015-12-06 00:39:03 -0500

    [#223] Moved templates into authenticated/anonymous front page structure

--- docroot/profiles/unallocated/themes/unallocated/templates/front-page-authenticated.tpl.php
+++ docroot/profiles/unallocated/themes/unallocated/templates/front-page-authenticated.tpl.php
@@ -8,6 +8,10 @@ $ColumnAWidth = '"five columns alpha"';
 $ColumnBWidth = '"eleven columns omega"';
 $ColumnBottomWidth ='"four columns"';
 
+
+//die('<pre>'.print_r(array_keys($page), TRUE).'</pre>');
+
+
 ?>
 
 <div id= 'top_zone'>
@@ -17,31 +21,50 @@ $ColumnBottomWidth ='"four columns"';
         <img id="logoImg" src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
       </a>
     <?php endif; ?>
+    <div id ='user-bar' class= 'container'>
+      <?php print render($page['user_bar_first']); ?>
+    </div>
+    <div id ='branding' class= 'container'>
+      <?php print render($page['branding']); ?>
+    </div>
 
   </div>
-  <div class= 'nav container'>
+  <div id ='main-nav' class= 'nav container'>
     <?php print render($page['main_nav']); ?>
   </div>
 </div>
 
 <div id='core' class="<?php print $fullWidth; ?>">
-  <div class="container">
-    <div id="front-core-left" class="eight columns"><?php print render($page['front_core_left']); ?></div>
-    <div id="front-core-right" class="eight columns"><?php print render($page['front_core_right']); ?></div>
+  <div class="container whiteShade">
+
+    <div id="front-page-main" class="twelve columns alpha"><?php print render($page['front_slideshow']); ?></div>
+    <div id ="front_slideshow" class="four columns omega"><?php print render($page['front_slideshow_sidebar']); ?></div>
+
   </div>
 </div>
 
-<div id="dashboard-groups" class="sixteen columns">
+<div id="first-break" class="sixteen columns break">
   <div class="container">
-    <div id="dashboard-groups-left" class="eight columns"><?php print render($page['dashboard_groups_left']); ?></div>
-    <div id="dashboard-groups-right" class="eight columns"><?php print render($page['dashboard_groups_right']); ?></div>
+    <div id="first-break-main" class="sixteen columns"><?php print render($page['first_break']); ?></div>
   </div>
 </div>
 
-<div id="dashboard-projects" class="sixteen columns">
+<div id="second-break" class="sixteen columns break">
   <div class="container">
-    <div id="dashboard-projects-left" class="eight columns"><?php print render($page['dashboard_projects_left']); ?></div>
-    <div id="dashboard-projects-right" class="eight columns"><?php print render($page['dashboard_projects_right']); ?></div>
+    <div id="second-break-main" class="sixteen columns"><?php print render($page['second_break']); ?></div>
+  </div>
+</div>
+
+<div id="front-page-secondary" class="sixteen columns whiteShade">
+  <div class="container">
+    <div id="front-page-secondary-left" class="eight columns alpha"><?php print render($page['front_second_left']); ?></div>
+    <div id="front-page-secondary-right" class="eight columns omega"><?php print render($page['front_second_right']); ?></div>
+  </div>
+</div>
+
+<div id="third-break" class="sixteen columns break">
+  <div class="container">
+    <div id="third-break-main" class="sixteen columns"><?php print render($page['third_break']); ?></div>
   </div>
 </div>
 
@@ -54,19 +77,20 @@ $ColumnBottomWidth ='"four columns"';
     </div>
 
 
-    <div class="<?php print $ColumnBottomWidth; ?>">
+    <div id ='post-script' class="<?php print $ColumnBottomWidth; ?>">
       <?php print render($page['postscript_first']); ?>
     </div>
 
 
-    <div class="<?php print $ColumnBottomWidth; ?>">
+    <div id ="post-post-script" class="<?php print $ColumnBottomWidth; ?>">
       <?php print render($page['post_postscript']); ?>
     </div>
 
 
-    <div class="<?php print $ColumnBottomWidth; ?>">
+    <div id ="post-post-post-script" class="<?php print $ColumnBottomWidth; ?>">
       <?php print render($page['post_post_postscript']); ?>
     </div>
 
   </div>
 </div>
+
